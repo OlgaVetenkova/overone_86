4314b3dc940abd494483ba6742697e75eb10b52d
Olga <92888189+OlgaVetenkova@users.noreply.github.com>
2021-10-29 20:07:41 +0300

    Update Hometask_функции_1.py

--- Hometask_функции_1.py
+++ Hometask_функции_1.py
@@ -1,14 +1,16 @@
 # Калькулятор
 
 def calc(num_1, operator, num_2):
+    res = 0
     if operator == "+":
-        print(num_1 + num_2)
+        res = num_1 + num_2
     elif operator == "-":
-        print(num_1 - num_2)
-    elif operator == "*":
-        print(num_1 * num_2)
+        res = num_1 - num_2
+    elif operator == "**":
+        res = num_1 * num_2
     elif operator == "/":
-        print(num_1 / num_2)
+        res = num_1 / num_2
+    return res
 num_1 = int(input("Введите значение 1"))
 num_2 = int(input("Введите значение 2"))
 operator = input("Введите знак операции (+, -, *, /)")
